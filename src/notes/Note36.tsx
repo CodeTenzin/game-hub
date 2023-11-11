70cbc487003eb5a7492b07eed4aad42ef6eef759
CoderX- <tenz_wang333@yahoo.com>
2023-11-11 04:37:29 -0500

    Delete src/notes directory

--- src/notes/Note36.tsx
+++
@@ -1,9 +0,0 @@
-// Final part
-// deploy our application on a platform called Vercel.
-// theres are other options to deply our React application.
-
-// https://vitejs.dev/guide/   or https://vitejs.dev/guide/static-deploy.html
-// Deploying A Static Site
-// details about a vite projects.
-
-// using Vercel in this project.
